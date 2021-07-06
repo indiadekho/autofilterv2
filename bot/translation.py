bfa9f4e52c7bc5d416f54a6a85d0d9daec1b5cf6
indiadekho <73731918+indiadekho@users.noreply.github.com>
2021-07-06 18:40:53 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,47 +4,6 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
-
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
-
-For More Details Click Help Button Below..
-@ITS_MY_LEECH
-</i>"""    
-    
-    HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <b>/add chat_id</b>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <b>/add @Username</b>
-     
-    -> <b>/del chat_id</b>
-                OR                  - To disconnect A Group With A Channel
-     <b>/del @Username</b>
-     
-    -> <b>/delall</b>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <b>/settings</b> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-@ITS_MY_LEECH
-"""
-    
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/URS_BOND">BOND</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
-"""
+    START_TEXT = """
+    YOU ARE NOT AUTHORISED TO USE ME
+    SORRY
